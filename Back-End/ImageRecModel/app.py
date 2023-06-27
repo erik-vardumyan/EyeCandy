f998454a8fcaae39d9eb112d68edb13214b779f7
Erik Vardumyan <100494443+erik-vardumyan@users.noreply.github.com>
2023-06-26 18:31:31 -0700

    Add files via upload

---
+++ Back-End/ImageRecModel/app.py
@@ -0,0 +1,169 @@
+# Needed imports for image recognition
+from flask import Flask, request, jsonify
+from keras.models import load_model
+from flask_cors import CORS
+from werkzeug.utils import secure_filename
+
+# Needed imports for color recognition
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import matplotlib.image as mpimg
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+import extcolors
+from colormap import rgb2hex
+
+# Needed imports for regular python use
+import cv2
+import os
+import base64
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+
+
+# Load the model outside the predict function
+model = load_model('clothing_classifier_model_v2.h5')
+
+# This will start our instance
+app = Flask(__name__)
+
+# This will enable cors that allows us to connect our flask to react
+CORS(app)
+
+# We are setting max size of file as 10mb
+app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
+
+# Our upload folder will be designated here and the allowed extensions as well
+UPLOAD_FOLDER = 'static/images'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# This function checks if the file uploaded is valid
+def allowed_files(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# Function to convert color extraction output to a DataFrame
+def color_to_df(input):
+    colors_pre_list = str(input).replace('([(','').split(', (')[0:-1]
+    df_rgb = [i.split('), ')[0] + ')' for i in colors_pre_list]
+    df_percent = [i.split('), ')[1].replace(')','') for i in colors_pre_list]
+    
+    # Convert RGB to HEX code
+    df_color_up = [rgb2hex(int(i.split(", ")[0].replace("(","")),
+                        int(i.split(", ")[1]),
+                        int(i.split(", ")[2].replace(")",""))) for i in df_rgb]
+    
+    df = pd.DataFrame(zip(df_color_up, df_percent), columns = ['c_code','occurence'])
+    return df
+
+
+# This function will pull the model and predict based on different classes
+@app.route('/predict', methods=['POST'])
+def predict():
+    # This ensures file path is not empty
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+
+    # So once the file path is determined -- We will convert images
+    if file and allowed_files(file.filename):
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        
+        # # Open the image using OpenCV for GrabCut
+        # img_cv2 = cv2.imread(file_path)
+        # mask = np.zeros(img_cv2.shape[:2], np.uint8)
+
+        # # Define a rectangle around the object of interest. 
+        # rect = (10, 10, img_cv2.shape[1] - 20, img_cv2.shape[0] - 20)
+
+        # bgdModel = np.zeros((1,65),np.float64)
+        # fgdModel = np.zeros((1,65),np.float64)
+
+        # # Run GrabCut
+        # cv2.grabCut(img_cv2, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
+
+        # # If mask==2 or mask==1, mask2 gets 0, otherwise it gets 1 as 'uint8'
+        # mask2 = np.where((mask==2)|(mask==0), 0, 1).astype('uint8')
+
+        # # Adding additional dimension for RGB to the mask, by default it gets 1 at last dimension.
+        # mask2 = np.repeat(mask2[:, :, np.newaxis], 3, axis=2)
+
+        # # Applying the mask to the image
+        # img_cv2 = img_cv2*mask2
+
+        # # Save the image for color extraction
+        # cv2.imwrite(file_path, img_cv2)
+
+        # They will convert to 28x28 grayscale images
+        img = Image.open(file_path).convert('L')
+        img = img.resize((28, 28), Image.ANTIALIAS)
+        img = np.array(img)
+        img = (255 - img) / 255.0
+        img = np.expand_dims(img, axis=0)
+        img = np.expand_dims(img, axis=-1)
+
+        class_prediction = model.predict(img)
+        predicted_label = int(np.argmax(class_prediction))
+
+        # Extract colors
+        colors_x = extcolors.extract_from_path(file_path, tolerance=12, limit=12)
+        df_color = color_to_df(colors_x)
+
+        # Get color codes and percentages
+        color_codes = list(df_color['c_code'])
+        color_percentages = [str(round(int(occurrence) * 100 / sum([int(occ) for occ in df_color['occurence']]), 1) + 20) + '%' for occurrence in df_color['occurence']]
+
+        color_dict = [{"color_code": color_codes[0]}]
+
+        color_dict += [{"color_code": code, "percentage": percentage} for code, percentage in zip(color_codes[1::], color_percentages[1::])]
+        
+        # These are the classes that will be used to determine the items uploaded
+        if predicted_label == 0:
+            product = "TShirt"
+        elif predicted_label == 1:
+            product = "Pants"
+        elif predicted_label == 2:
+            product = "T-Shirt Loose"
+        elif predicted_label == 3:
+            product = "Dress"
+        elif predicted_label == 4:
+            product = "Coat"
+        elif predicted_label == 5:
+            product = "Shoes"
+        elif predicted_label == 6:
+            product = "Shirt"
+        elif predicted_label == 7:
+            product = "Sneaker"
+        elif predicted_label == 8:
+            product = "Bag"
+        else:
+            product = "Ankle Boot"
+
+        # This will allow us to upload images and show them properly on the front end page using base64 string formatting
+        with open(file_path, "rb") as f:
+            user_image = base64.b64encode(f.read()).decode("utf-8")
+
+        os.remove(file_path)
+
+        # This will use JSONIFY to generate our output and the image, the same that we will use with our color model
+        return jsonify({
+            'product': product,
+            'user_image': f"data:image/jpeg;base64,{user_image}",
+            'colors': color_dict,
+        })
+
+    # Outputs error messages if something is input, uploaded, or mismatched wrong
+    return jsonify({'error': 'Invalid file extension'}), 400
+
+# The way our flask app runs, runs with NPM RUN START:BOTH
+if __name__ == '__main__':
+    app.run(debug=True)
